99d689aa3321e3a273031490fbade64c3ddc9e3d
Linnea <linneamadsen@gmail.com>
2022-06-07 08:51:53 -0500

    Create AppleAndOrange.js

---
+++ AppleAndOrange.js
@@ -0,0 +1,82 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countApplesAndOranges' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER s
+ *  2. INTEGER t
+ *  3. INTEGER a
+ *  4. INTEGER b
+ *  5. INTEGER_ARRAY apples
+ *  6. INTEGER_ARRAY oranges
+ */
+
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    // Write your code here
+    var distance;
+    var apples_hit_house = 0;
+    var oranges_hit_house = 0;
+    for (var i = 0; i<apples.length; i++){
+        distance = apples[i] + a;
+        if (distance >= s && distance <= t){
+            apples_hit_house += 1;
+        }
+    }
+    for (var i = 0; i<oranges.length; i++){
+        distance = oranges[i] + b;
+        if (distance >= s && distance <= t){
+            oranges_hit_house += 1;
+        }
+    }
+    
+    console.log(apples_hit_house);
+    console.log(oranges_hit_house);
+    
+}
+
+function main() {
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const s = parseInt(firstMultipleInput[0], 10);
+
+    const t = parseInt(firstMultipleInput[1], 10);
+
+    const secondMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const a = parseInt(secondMultipleInput[0], 10);
+
+    const b = parseInt(secondMultipleInput[1], 10);
+
+    const thirdMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const m = parseInt(thirdMultipleInput[0], 10);
+
+    const n = parseInt(thirdMultipleInput[1], 10);
+
+    const apples = readLine().replace(/\s+$/g, '').split(' ').map(applesTemp => parseInt(applesTemp, 10));
+
+    const oranges = readLine().replace(/\s+$/g, '').split(' ').map(orangesTemp => parseInt(orangesTemp, 10));
+
+    countApplesAndOranges(s, t, a, b, apples, oranges);
+}
